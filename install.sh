c3a6b0966f6578a8482547c54b1235934fcd86fc
Geoffrey Sneddon <me@gsnedders.com>
2017-12-06 17:13:26 +0000

    Output available updates

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4
 SAFARI_VERSION=$(defaults read /Applications/Safari.app/Contents/Info.plist CFBundleShortVersionString)
 
 if [ $(version $SAFARI_VERSION) -le $(version 11) ]; then
+    softwareupdate -l
     sudo softwareupdate -i Safari
     SAFARI_VERSION=$(defaults read /Applications/Safari.app/Contents/Info.plist CFBundleShortVersionString)
 fi
